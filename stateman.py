426a36b7d49ae3e919542a578722d21045ec7ebd
hakver29 <haakongv@stud.ntnu.no>
2018-10-14 17:19:27 +0200

    add stuff

--- stateman.py
+++ stateman.py
@@ -14,8 +14,61 @@ class Node(object):
         self.data = data
         self.children = []
 
+        self.numerator = 0
+        self. denominator = 0
+
     def add_child(self, obj):
         self.children.append(obj)
 
-class Stateman:
-    
+
+class Stateman(object):
+    def __init__(self,data):
+
+class Board(object):
+    def start(self):
+        # Returns a representation of the starting state of the game.
+        pass
+
+    def current_player(self, state):
+        # Takes the game state and returns the current player's
+        # number.
+        pass
+
+    def next_state(self, state, play):
+        # Takes the game state, and the move to be applied.
+        # Returns the new game state.
+        pass
+
+    def legal_plays(self, state_history):
+        # Takes a sequence of game states representing the full
+        # game history, and returns the full list of moves that
+        # are legal plays for the current player.
+        pass
+
+    def winner(self, state_history):
+        # Takes a sequence of game states representing the full
+        # game history.  If the game is now won, return the player
+        # number.  If the game is still ongoing, return zero.  If
+        # the game is tied, return a different distinct value, e.g. -1.
+        pass
+
+class MonteCarlo(object):
+    def __init__(self, board, **kwargs):
+        # Takes an instance of a Board and optionally some keyword
+        # arguments.  Initializes the list of game states and the
+        # statistics tables.
+        pass
+
+    def update(self, state):
+        # Takes a game state, and appends it to the history.
+        pass
+
+    def get_play(self):
+        # Causes the AI to calculate the best move from the
+        # current game state and return it.
+        pass
+
+    def run_simulation(self):
+        # Plays out a "random" game from the current position,
+        # then updates the statistics tables with the result.
+        pass
