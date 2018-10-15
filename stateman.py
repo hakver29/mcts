0f63e8c87c26d91a2989f2ace2b601acb4f2031a
hakver29 <haakongv@stud.ntnu.no>
2018-10-15 14:04:35 +0200

    add

--- stateman.py
+++ stateman.py
@@ -9,14 +9,46 @@ the default policy from the leaf node’s state to a final state.
 course lecture notes) at all nodes and edges on the path from the final state to the tree root.
 """
 import math
-class Statemanager(object):
-    def __init__(self,data):
 
-class Nim(object):
+class Statemanager(object):
+    # Litt uklart hva som skal ligge
     def __init__(self,data):
+        pass
+
+class NimGame(object):
+    def __init__(self,G,P,M,N,K, verbose):
+        self.G = G
+        self.P = P
+        self.M = M
+        self.N = N
+        self.K = K
+        self.verbose_mode = verbose
+
+class NimState(object):
+    def __init__(self, chips):
+        self.chips = chips
+
+    def Actions(self, K, chips):
+        if chips >= K:
+            return [i for i in range(1,K+1)]
+        else:
+            return [i for i in range(1,chips+1)]
+
+    def DoAction(self,action):
+        self.chips -= action
 
 class Node(object):
-    def __init__(self,data):
+    def __init__(self,move, parent, state):
+        self.move = move
+        self.parentNode = parent
+        self.childNodes = []
+        self.wins = 0
+        self.visits = 0
+        self.untriedMoves = state.Actions()
+
+    def add_child(self, move, state):
+        node = Node(move = move, parent = self, state = state)
+        self.childNodes.append(node)
 
 class UCT(object):
     def __init__(self,data):
