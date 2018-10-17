97355caf6bec2f54182a5d9b72eb56a96a4d250b
magnus <magnublo@stud.ntnu.no>
2018-10-17 12:57:47 +0200

    m

---
+++ GameSetting.py
@@ -0,0 +1,10 @@
+class GameSetting:
+    def __init__(self, G=0, P=0, M=0, N=0, K=0, verbose=False):
+        self.G = G
+        self.P = P
+        self.M = M
+        self.N = N
+        self.K = K
+        self.verbose = verbose
+
+
---
+++ NimState.py
@@ -0,0 +1,50 @@
+class NimState:
+    """ A state of the game Nim. In Nim, players alternately take 1,2 or 3 chips with the
+        winner being the player to take the last chip.
+        In Nim any initial state of the form 4n+k for k = 1,2,3 is a win for player 1
+        (by choosing k) chips.
+        Any initial state of the form 4n is a win for player 2.
+    """
+
+    def __init__(self, game_setting):
+        # self.playerJustMoved = 2  # At the root pretend the player just moved is p2 - p1 has the first move
+        # self.chips = ch
+        self.stones_remaining = game_setting.N
+        self.playerJustMoved = game_setting.P
+        self.game_setting = game_setting
+
+    def Clone(self):
+        """ Create a deep clone of this game state.
+        """
+        st = NimState(self.game_setting)
+        st.playerJustMoved = self.playerJustMoved
+        return st
+
+    def DoMove(self, move):
+        """ Update a state by carrying out the given move.
+            Must update playerJustMoved.
+        """
+        # assert move >= 1 and move <= 3 and move == int(move)
+        self.stones_remaining -= move
+        self.playerJustMoved = 3 - self.playerJustMoved
+
+    def GetMoves(self):
+        """ Get all possible moves from this state.
+        """
+        K = self.game_setting.K
+
+        if K > self.stones_remaining:
+            return [i for i in range(1, self.stones_remaining + 1)]
+        else:
+            return [i for i in range(1, K + 1)]
+
+        # return [i for i in range(1, min([4, self.stones_remaining + 1]))]
+
+    def GetResult(self, playerjm):
+        """ Get the game result from the viewpoint of playerjm.
+        """
+        assert self.stones_remaining == 0
+        if self.playerJustMoved == playerjm:
+            return 1.0  # playerjm took the last chip and has won
+        else:
+            return 0.0  # playerjm's opponent took the last chip and has won
