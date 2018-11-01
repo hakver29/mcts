17e4df16210e8c14a735d0f15014135eb2b09939
hakver29 <haakongv@stud.ntnu.no>
2018-11-01 14:45:48 +0100

    add

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ def tree_search(rootstate, itermax, verbose=False):
         state = rootstate.clone()
 
         """
-        Selection: start from root R and select successive child nodes until a leaf node L is reached. The root is the current game state and a leaf is any node from which no simulation (playout) has yet been initiated. The section below says more about a way of biasing choice of child nodes that lets the game tree expand towards the most promising moves, which is the essence of Monte Carlo tree search.
+        Selection: start from root R and select successive child nodes until a leaf node L is reached. 
+        The root is the current game state and a leaf is any node from which no simulation (playout) has yet been initiated. 
+        The section below says more about a way of biasing choice of child nodes that lets the game tree expand towards 
+        the most promising moves, which is the essence of Monte Carlo tree search.
         
-        Expansion: unless L ends the game decisively (e.g. win/loss/draw) for either player, create one (or more) child nodes and choose node C from one of them. Child nodes are any valid moves from the game position defined by L.
+        Expansion: unless L ends the game decisively (e.g. win/loss/draw) for either player, 
+        create one (or more) child nodes and choose node C from one of them. 
+        Child nodes are any valid moves from the game position defined by L.
         
-        Simulation: complete one random playout from node C. This step is sometimes also called playout or rollout. A playout may be as simple as choosing uniform random moves until the game is decided (for example in chess, the game is won, lost, or drawn).
+        Simulation: complete one random playout from node C. This step is sometimes also called playout or rollout. 
+        A playout may be as simple as choosing uniform random moves until the game is decided (for example in chess, 
+        the game is won, lost, or drawn).
         
         Backpropagation: use the result of the playout to update information in the nodes on the path from C to R.
         """
@@ -45,10 +52,8 @@ def tree_search(rootstate, itermax, verbose=False):
 
 
 def play_game(game_setting):
-    """ Play a sample game between two UCT players where each player gets a different number
-        of UCT iterations (= simulations = tree nodes).
-
-        Spiller et enkelt spill mellom to spillere
+    """
+    Spiller et enkelt spill mellom to spillere
     """
     player_wins = [0,0]
     for i in range(game_setting.G):
